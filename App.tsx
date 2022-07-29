f79c698d1afd415c608444ad545234fb29bb6cb0
Bryan Lee <bryanmylee@fb.com>
2022-07-29 16:27:39 +0800

    feat: custom bottom sheet with gesture handler

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import BasicScreen from "./BasicScreen";
 import PanGestureScreen from "./PanGestureScreen";
 import ScrollViewScreen from "./ScrollViewScreen";
 import ColorsScreen from "./ColorsScreen";
+import BottomSheetScreen from "./BottomSheetScreen";
 
 export type RootDrawerParamList = {
 	Basic: undefined;
 	PanGesture: undefined;
 	ScrollView: undefined;
 	Colors: undefined;
+	BottomSheet: undefined;
 };
 
 const RootDrawer = createDrawerNavigator<RootDrawerParamList>();
@@ -24,6 +26,7 @@ export default function App() {
 				<RootDrawer.Screen name="PanGesture" component={PanGestureScreen} />
 				<RootDrawer.Screen name="ScrollView" component={ScrollViewScreen} />
 				<RootDrawer.Screen name="Colors" component={ColorsScreen} />
+				<RootDrawer.Screen name="BottomSheet" component={BottomSheetScreen} />
 			</RootDrawer.Navigator>
 		</NavigationContainer>
 	);
---
+++ BottomSheetScreen.tsx
@@ -0,0 +1,125 @@
+import { useEffect } from "react";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
+import {
+	Gesture,
+	GestureDetector,
+	GestureHandlerRootView,
+	PanGestureHandler,
+	PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+	Extrapolate,
+	interpolate,
+	useAnimatedGestureHandler,
+	useAnimatedStyle,
+	useSharedValue,
+	withDecay,
+	withSequence,
+	withSpring,
+} from "react-native-reanimated";
+import { clamp, clampWorklet } from "./clamp";
+
+export default function BottomSheetScreen() {
+	return (
+		<GestureHandlerRootView style={styles.rootView}>
+			<View style={[styles.container]}>
+				<BottomSheet />
+			</View>
+		</GestureHandlerRootView>
+	);
+}
+
+const styles = StyleSheet.create({
+	rootView: {
+		flex: 1,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: "black",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
+
+type TPanContext = {
+	initialY: number;
+};
+
+function BottomSheet() {
+	const { height } = useWindowDimensions();
+
+	const translateY = useSharedValue(0);
+	useEffect(function showBottomSheetOnLoad() {
+		translateY.value = withSpring(-height / 3);
+	}, []);
+
+	const handlePanGesture = useAnimatedGestureHandler<
+		PanGestureHandlerGestureEvent,
+		TPanContext
+	>({
+		onStart(_, context) {
+			context.initialY = translateY.value;
+		},
+		onActive(event, context) {
+			translateY.value = clampWorklet(
+				context.initialY + event.translationY,
+				-height,
+				-height / 3
+			);
+		},
+		onEnd(event) {
+			const targetY =
+				translateY.value + event.velocityY < (-height * 2) / 3
+					? -height
+					: -height / 3;
+			translateY.value = withSpring(targetY, {
+				damping: 20,
+				velocity: event.velocityY,
+				overshootClamping: true,
+			});
+		},
+	});
+
+	const containerAnim = useAnimatedStyle(
+		() => ({
+			borderRadius: interpolate(
+				translateY.value,
+				[-height, -height / 3],
+				[0, 25],
+				Extrapolate.CLAMP
+			),
+			transform: [{ translateY: translateY.value }],
+		}),
+		[]
+	);
+
+	return (
+		<PanGestureHandler onGestureEvent={handlePanGesture}>
+			<Animated.View
+				style={[
+					bottomSheetStyles.container,
+					{ height, top: height },
+					containerAnim,
+				]}
+			>
+				<View style={bottomSheetStyles.handle} />
+			</Animated.View>
+		</PanGestureHandler>
+	);
+}
+
+const bottomSheetStyles = StyleSheet.create({
+	container: {
+		width: "100%",
+		backgroundColor: "white",
+		position: "absolute",
+	},
+	handle: {
+		width: 75,
+		height: 4,
+		backgroundColor: "grey",
+		alignSelf: "center",
+		marginVertical: 15,
+		borderRadius: 2,
+	},
+});
